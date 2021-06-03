08cb9da5f987cc73f6b484e3e46d994cbfdfa57a
Mirko J <mirko@localhost.localdomain>
2021-06-02 19:37:08 -0500

    Add the first Atari environment in OpenAI.

---
+++ OpenAI-Atari_RL.py
@@ -0,0 +1,80 @@
+# ==================================
+# Crear un modelo de RL para el juego
+# ===================================
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# https://towardsdatascience.com/reinforcement-learning-with-openai-d445c2c687d2
+# https://miro.medium.com/max/926/0*7PvoCPJuUAblMdcn.png
+
+import gym
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Convolution2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+
+# Environment: Space Invaders
+env = gym.make('SpaceInvaders-v0')
+
+
+# Crear un modelo DL como base del agente
+def build_model(height, width, channels, actions):
+    cnn_model = Sequential()
+    cnn_model.add(Convolution2D(32, (8, 8), strides=(4, 4), activation='relu', input_shape=(3, height, width, channels)))
+    cnn_model.add(Convolution2D(64, (4, 4), strides=(2, 2), activation='relu'))
+    cnn_model.add(Convolution2D(64, (3, 3), activation='relu'))
+    cnn_model.add(Flatten())
+    cnn_model.add(Dense(512, activation='relu'))
+    cnn_model.add(Dense(256, activation='relu'))
+    cnn_model.add(Dense(actions, activation='linear'))
+    return cnn_model
+
+
+# Observaciones
+height, width, channels = env.observation_space.shape
+# Acciones
+actions = env.action_space.n
+model = build_model(height, width, channels, actions)
+model.summary()
+
+# Construyendo el Agente con Keras-RL
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+
+
+def build_agent(model, actions, policy):
+    dqn = DQNAgent(model=model, policy=policy, nb_actions=actions, memory=SequentialMemory(limit=1000, window_length=3),
+                   enable_dueling_network=True, dueling_type='avg', nb_steps_warmup=1000)
+    return dqn
+
+
+# Policy
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.2, nb_steps=10000)
+# Agent
+agent = build_agent(model, actions, policy)
+
+
+# Train the model
+agent.compile(Adam(lr=1e-4))
+agent.fit(env, nb_steps=10000, visualize=False, verbose=2)
+
+
+# Save the model
+agent.save_weights('model_test/dqn_atari_agent_weights.h5f')
+
+# =============================
+# Cargar el modelo RL entrenado
+# =============================
+
+# Load the model
+model_name = 'dqn_atari_agent_weights.h5f'
+agent.load_weights('model_test/' + model_name)
+print("Model {} loaded ...".format(model_name))
+
+# Test de model
+scores = agent.test(env, nb_episodes=5, visualize=True)
+print(np.mean(scores.history['episode_reward']))
---
+++ OpenAI-Atari_TestEnv.py
@@ -0,0 +1,32 @@
+# https://gym.openai.com/envs/SpaceInvaders-v0/
+
+# 1. Evaluar un Environment con OpenAI Gym (Acciones aleatorias)
+import gym
+import random
+
+# Environment: Space Invaders
+env = gym.make('SpaceInvaders-v0')
+height, width, channels = env.observation_space.shape
+print ("Observation space: ", env.observation_space)
+
+# Actions
+actions = env.action_space.n
+action_meanings = env.unwrapped.get_action_meanings()
+print ("Action space: ", env.action_space)
+print ("Action meanings: ", action_meanings)
+
+# Episodes
+episodes = 20
+for episode in range(episodes):
+    state = env.reset()
+    done = False
+    score = 0
+
+    # done=True cuando es destruido
+    while not done:
+        env.render()
+        action = random.choice([0, 1, 2, 3, 4, 5])
+        n_state, reward, done, info = env.step(action)
+        score += reward
+    print('Episode:{} Score:{}'.format(episode+1, score))
+env.close()
